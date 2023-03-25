a12daaa13c912c9a305a141c236f8937d3148dd3
Gleb Sorvenkov <vajgleb03@gmail.com>
2023-03-25 21:54:16 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import './App.css';
+import { useState } from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+
+function MediaCard({ tag }) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardContent>
+        <Typography variant="h5" gutterBottom component="div">
+          Tag: {tag}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+function CardList( { tags } ) {
+  const cardList = tags.map((tag, index) => {
+    return (
+      <li key={index}>
+        <MediaCard tag={tag}/>
+      </li>
+    );
+  });
+
+  return <ul>{cardList}</ul>;
+
+}
+
+function TagSearch( { tags, selectedTags } ) {
+  return (
+    <Autocomplete
+      multiple
+      limitTags={2}
+      id="multiple-limit-tags"
+      options={tags}
+      getOptionLabel={(tag) => tag}
+      renderInput={(params) => (
+        <TextField {...params} label="limitTags" placeholder="Favorites" />
+      )}
+      sx={{ width: '500px' }}
+    />
+  );
+}
+
+function App() {
+  const tags = ['one', 'two', 'three'];
+  const [ selectedTags, setSelectedTags ] = useState(['one', 'two', 'three']);
+
+  return (
+    <>
+      <TagSearch tags={tags} selectedTags={selectedTags} />
+    </>
+  );
+}
+
+export default App;
